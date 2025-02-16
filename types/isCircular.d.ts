3219c9bc43f5eb8e051b7ac50ee59af21e9b797f
commenthol <commenthol@gmail.com>
2025-02-16 09:39:02 +0100

    docs: fix isCircular type

--- types/isCircular.d.ts
+++ types/isCircular.d.ts
@@ -1,1 +1,14 @@
+/**
+ * check if an object `obj` contains circular structures
+ *
+ * @example
+ * ````js
+ * import { isCircular } from 'mergee'
+ * const obj = { a: {} };
+ * obj.a.c = { c: 1 };
+ * //> isCircular(obj) === true
+ * ````
+ * @param {object} obj - Object to check
+ * @return {boolean} true if `obj` is circular
+ */
 export function isCircular(obj: object): boolean;
